d176a622773f111ea9164bf200f2559e21377b75
Amrita Mahesh <74362099+amrita-mahesh@users.noreply.github.com>
2025-05-05 19:15:11 +0530

    Update UMAP_analysis.R

--- UMAP_analysis.R
+++ UMAP_analysis.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(org.Hs.eg.db)
 
 
-setwd("D:/Projects/Github side projects/scRNAseq analysis/Data/")
+setwd("D:/Projects/Github projects/scRNAseq analysis/Data/")
 
 #Load input data
 expression_matrix <- ReadMtx(mtx = "matrix.mtx.gz", cells = "barcodes.tsv.gz", features = "features.tsv.gz", feature.column = 1)
@@ -13,6 +13,7 @@ gene_keys <- expression_matrix@Dimnames[[1]]
 mapped_genes <- data.frame(mapIds(org.Hs.eg.db, keys = gene_keys, column = "SYMBOL", keytype = "ENSEMBL", multiVals = "first"))
 mapped_genes$ENSEMBL <- rownames(mapped_genes)
 colnames(mapped_genes) <- c("Symbol", "ENSEMBL")
+
 #Accounting for duplicated or unmapped gene symbols
 mapped_genes <- mapped_genes %>% 
   mutate(mapped_names = if_else((is.na(Symbol) | duplicated(Symbol)), ENSEMBL, Symbol))
@@ -27,7 +28,7 @@ expression_matrix <- NULL
 seurat_object <- NormalizeData(seurat_object, normalization.method = "LogNormalize")
 seurat_object <- FindVariableFeatures(seurat_object, selection.method = "vst", nfeatures = 2000)
 
-#Plot the top 20 most variable genes
+#Plot the top 5 most variable genes
 most_variable <- head(VariableFeatures(seurat_object), 5)
 scatter_plot_genes <- VariableFeaturePlot(seurat_object)
 LabelPoints(plot = scatter_plot_genes, points = most_variable, repel = TRUE, xnudge = 0, ynudge = 0)
